9c7bc5f43fe263739d5c9412be8511e16d88aea1
Brian Cavalier <brian@briancavalier.com>
2020-08-21 13:51:42 -0400

    Remove and ignore dist

--- dist/property.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.prop = void 0;
-const array_1 = require("./array");
-exports.prop = (f, p, ...sources) => (n) => array_1.cartesian(sources.map(s => s(n))).map(args => {
-    const result = f(...args);
-    return { depth: n, input: args, result, ok: p(result, ...args) };
-});
